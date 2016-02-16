e24945b73b1de90b6ec711ebd35a38096909b17e
Roxana Ungureanu <ungureanu.roxi@yahoo.com>
2016-02-16 12:02:12 +0200

    react first commit

---
+++ src/components/ActiveUsers.js
@@ -0,0 +1,28 @@
+require('normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+import User from './User';
+
+var ActiveUsers = React.createClass({
+  getDefaultProps: function () {
+    return {
+      list: []
+    }
+  },
+
+  render: function () {
+
+    var activeUsers = this.props.list.map(function (user, idx) {
+      return (<User key={idx} firstName={user.first_name} lastName={user.last_name} />);
+    });
+
+    return (
+      <div className="active-users">
+        {activeUsers}
+      </div>
+    );
+  }
+});
+
+export default ActiveUsers;
